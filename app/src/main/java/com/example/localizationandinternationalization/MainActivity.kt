e15fe8b1987865bb9495a65707c5eca2ac569a24
Oluwafemi Ogundipe <femiogundipe01@gmail.com>
2022-05-30 21:26:46 +0100

    added animations

--- app/src/main/java/com/example/localizationandinternationalization/MainActivity.kt
+++ app/src/main/java/com/example/localizationandinternationalization/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.localizationandinternationalization
 
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<TextView>(R.id.text_view)
+        val animator = ValueAnimator.ofInt(0, 600)
+        animator.duration = 100000 // 5 seconds
+        animator.addUpdateListener { animation ->
+            view.text = animation.animatedValue.toString()
+        }
+        animator.start()
     }
+
+
 }
